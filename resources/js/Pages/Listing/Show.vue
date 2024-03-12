2e5c4e853ae143bd820233532fc2b2f7e2e42446
parvazreza00 <parvazreza00@gmail.com>
2024-03-12 11:16:33 +0600

    listing vues and components vue created

---
+++ resources/js/Pages/Listing/Show.vue
@@ -0,0 +1,12 @@
+<template>
+
+<ListingAddress :listing="listing"/>
+
+</template>
+
+<script setup>
+import ListingAddress from '@/Components/ListingAddress.vue'
+defineProps({
+    listing: Object
+})
+</script>
